d94acfabe8c4b6fdf135a49d828def98a6662e0e
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-05-09 13:27:09 -0600

    initial commit

--- elevator.js
+++ elevator.js
@@ -1,7 +1,84 @@
-export default class Elevator {
+class Elevator {
   constructor() {
+    this.currentFloor = 0,
+    this.floorsTraversed = 0,
+    this.stops = [],
+    this.queue = [],
+    this.currentRiders = [],
+    this.motionStatus = "idle"
   }
 
-  get currentFloor() {
+  currentFloor() {
+    return this.currentFloor;
   }
-}
+
+  goToFloor(person) {
+    this.queue.push(person)
+
+    this.checkQueue()
+  }
+
+  checkQueue() {
+    // BREAK OUT MORE!!! TOO CONNECTED NEED TO BE ABLE TO TEST
+    if (this.queue.length > 0) {
+      // change to moving
+      this.motionStatus = "moving";
+      // moving to current floor of person
+      this.currentFloor = this.queue[0].currentFloor;
+      // adding stop to array
+      this.stops.push(this.currentFloor);
+      // add rider to riders array
+      this.currentRiders.push(this.queue[0])
+      // drop off person
+      this.currentFloor = this.queue[0].dropOffFloor;
+      // add stop to array
+      this.stops.push(this.currentFloor);
+      // take rider out of array
+      this.currentRiders.pop()
+      // remove from que
+      this.queue.splice(0,1)
+      // change status to idle again? maybe here?
+      this.motionStatus = "idle";
+    } else if (this.queue.length === 0) {
+      this.motionStatus = "idle"
+      // possibly send back to floor 0???
+    }
+
+  }
+
+  getStops() {
+    this.getTraversed(this.stops)
+    return this.stops
+  }
+
+  getTraversed(array) {
+    let x = 0
+    array.unshift(0)
+
+    for(let i = (array.length-1); i>=1; --i ) {
+      x = x + Math.abs(array[i]-array[i-1])
+    }
+    this.stops.splice(0,1)
+    return this.floorsTraversed = x
+  }
+
+
+  reset() {
+    this.currentFloor = 0,
+    this.floorsTraversed = 0,
+    this.stops = [],
+    this.queue = [],
+    this.currentRiders = [],
+    this.motionStatus = "idle"
+  }
+};
+
+class Person {
+  constructor(name, currentFloor, dropOffFloor) {
+    this.name = name,
+    this.currentFloor = currentFloor,
+    this.dropOffFloor = dropOffFloor
+  }
+};
+
+module.exports = { Elevator, Person };
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -1,8 +1,9 @@
 require('babel-core/register')({
   ignore: /node_modules\/(?!ProjectB)/
 });
+let assert = require('assert');
 
-const Elevator = require('../elevator').default;
+const { Elevator, Person } = require('../elevator')
 
 describe('Elevator', function() {
   let elevator = new Elevator();
@@ -12,20 +13,123 @@ describe('Elevator', function() {
   });
 
   it('should bring a rider to a floor above their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 2, dropOffFloor: 5 };
-    elevator.goToFloor(mockUser);
+    // let mockUser = { name: "Brittany", currentFloor: 2, dropOffFloor: 5 };
 
-    assert(elevator.currentFloor).equals(5);
-    assert(elevator.motionStatus).equals('idle');
-    assert(elevator.getStops()).equals([2, 5]);
+    let person = new Person("Brittany", 2,  5);
+    elevator.goToFloor(person);
+
+    assert.equal(elevator.currentFloor, 5);
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [2, 5]);
   });
 
   it('should bring a rider to a floor below their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 8, dropOffFloor: 3 };
-    elevator.goToFloor(mockUser);
+    // let mockUser = { name: "Brittany", currentFloor: 8, dropOffFloor: 3 };
 
-    assert(elevator.currentFloor).equals(3);
-    assert(elevator.motionStatus).equals('idle');
-    assert(elevator.getStops()).equals([8, 3]);
+    let person = new Person("Brittany", 8,  3);
+
+    elevator.goToFloor(person);
+
+    assert.equal(elevator.currentFloor, 3);
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [8, 3]);
   });
+
+  it('should calculate floorsTraversed, going up', () => {
+    let person = new Person("Brittany", 2,  7);
+
+    elevator.goToFloor(person);
+
+    assert.equal(elevator.currentFloor, 7);
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [2, 7]);
+    assert.equal(elevator.floorsTraversed, 7);
+  });
+
+  it('should calculate floorsTraversed, going down', () => {
+    let person = new Person("Brittany", 8,  3);
+
+    elevator.goToFloor(person);
+
+    assert.equal(elevator.currentFloor, 3);
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [8, 3]);
+    assert.equal(elevator.floorsTraversed, 13);
+  });
+
+  it('should have a reset method', () => {
+    let person = new Person("Brittany", 8,  3);
+
+    elevator.goToFloor(person);
+
+    assert.equal(elevator.currentFloor, 3);
+    assert.deepEqual(elevator.getStops(), [8, 3]);
+    assert.equal(elevator.floorsTraversed, 13);
+
+    elevator.reset();
+
+    assert.equal(elevator.currentFloor, 0);
+    assert.deepEqual(elevator.getStops(), []);
+    assert.equal(elevator.floorsTraversed, 0);
+    assert.deepEqual(elevator.currentRiders, []);
+    assert.deepEqual(elevator.queue, []);
+
+  });
+
+  it('should have a getStops method', () => {
+    let person = new Person("Brittany", 8,  3);
+    let person2 = new Person("Robbie", 8,  1);
+
+    elevator.goToFloor(person);
+
+    assert.deepEqual(elevator.getStops(), [8, 3]);
+
+    elevator.goToFloor(person2);
+    assert.deepEqual(elevator.getStops(), [8, 3, 8, 1]);
+  });
+
+  it('should have a currentFloor method', () => {
+    let person = new Person("Brittany", 8,  3);
+
+    assert.equal(elevator.currentFloor, 0);
+
+    elevator.goToFloor(person);
+
+    assert.deepEqual(elevator.getStops(), [8, 3]);
+    assert.equal(elevator.currentFloor, 3);
+  });
+
+  it('should have a getTraversed method', () => {
+    let person = new Person("Brittany", 8,  3);
+    let person2 = new Person("Robbie", 8,  1);
+
+    elevator.goToFloor(person);
+
+    assert.deepEqual(elevator.getStops(), [8, 3]);
+    assert.equal(elevator.getTraversed(elevator.stops), 13);
+    assert.equal(elevator.floorsTraversed, 13);
+
+    elevator.goToFloor(person2);
+    assert.deepEqual(elevator.getStops(), [8, 3, 8, 1]);
+    assert.equal(elevator.getTraversed(elevator.stops), 25);
+    assert.equal(elevator.floorsTraversed, 25);
+  });
+
+  it('should have a checkQueue method', () => {
+    let person = new Person("Brittany", 8,  3);
+
+    elevator.queue.push(person);
+
+    assert.equal(elevator.queue[0].name, person.name);
+    assert.equal(elevator.queue[0].currentFloor, person.currentFloor);
+    assert.equal(elevator.queue[0].dropOffFloor, person.dropOffFloor);
+
+    elevator.checkQueue();
+
+    assert.deepEqual(elevator.queue, []);
+    assert.deepEqual(elevator.getStops(), [8, 3]);
+    assert.deepEqual(elevator.motionStatus, 'idle');
+  });
+
+
 });
